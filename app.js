4c12e210ca5494358379d5a3cad52324705000a3
gafiyatovzz <54131452+gafiyatovzz@users.noreply.github.com>
2020-04-24 15:48:48 +0300

    review #2

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 const express = require('express');
 
-const app = express();
 const { PORT = 3000, BASE_PATH } = process.env;
+
+const app = express();
 const bodyParser = require('body-parser');
 const path = require('path');
 
-const router = require('./routes/routes');
+const routes = require('./routes/routes');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/', router);
+app.use('/', routes);
 
-app.use(express.static(path.join(__dirname, '/public'), { encoding: 'utf8' }));
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.listen(PORT, () => {
   console.log('Your port 3000');
