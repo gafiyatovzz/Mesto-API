4a2adb381ef41359d01d1c0a237da6cc0bb36114
gafiyatovzz <54131452+gafiyatovzz@users.noreply.github.com>
2020-05-30 11:58:19 +0300

    удаление исходников

--- src/main.js
+++
@@ -1,8 +0,0 @@
-require('./pages/index.css');
-require('./js/Card');
-require('./js/CardList');
-require('./js/FormValidation');
-require('./js/Popup');
-require('./js/UserInfo');
-require('./js/Api');
-require('./js/script');
