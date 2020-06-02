793ed3d84655ffd4621db86ccf4228f681a506bd
gafiyatovzz <54131452+gafiyatovzz@users.noreply.github.com>
2020-06-02 16:05:28 +0300

    ignore client

--- client/src/main.js
+++
@@ -1,9 +0,0 @@
-require('./pages/index.css');
-require('./js/Card');
-require('./js/CardList');
-require('./js/Popup');
-require('./js/FormValidation');
-require('./js/UserInfo');
-require('./js/Api');
-require('./js/script');
-
