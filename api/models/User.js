66e6025d846ec1a3f45c221b367fcf08f6c7a85d
gafiyatovzz <54131452+gafiyatovzz@users.noreply.github.com>
2020-06-08 12:58:58 +0300

    добавил к модели пользователя поля email и password, провалидировал поле email

--- api/models/User.js
+++ api/models/User.js
@@ -29,6 +29,20 @@ const userSchema = new mongoose.Schema({
       validator: (value) => /http[s]?:\/\/(www\.)?((\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})|(\w+\.[a-zA-Z]{2,6}))(:\d{2,5})?(\/[a-zA-Z0-9\/]*)?#?/i.test(value),
     },
   },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    validate: {
+      // eslint-disable-next-line
+      validator: (value) => /[\w\-]+@[\w\-]+\.[\w\-^0-9]+/.test(value),
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
+  },
 });
 
 module.exports = mongoose.model('user', userSchema);
