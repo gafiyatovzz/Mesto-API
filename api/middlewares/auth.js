c658e004987d535326fb9ec3d3f9b860ac9c9bb8
gafiyatovzz <54131452+gafiyatovzz@users.noreply.github.com>
2020-06-08 14:08:46 +0300

    миддлвэр авторизации + её защита

---
+++ api/middlewares/auth.js
@@ -0,0 +1,26 @@
+const jwt = require('jsonwebtoken');
+
+// eslint-disable-next-line consistent-return
+module.exports = (req, res, next) => {
+  const { authorization } = req.headers;
+
+  if (!authorization || !authorization.startWidth('Bearer ')) {
+    return res
+      .status(401)
+      .send({ message: 'Необходима авторизация' });
+  }
+
+  const token = authorization.replace('Bearer ', '');
+  let payload;
+
+  try {
+    payload = jwt.verify(token, 'super-strong-secret');
+  } catch (err) {
+    return res
+      .status(401)
+      .send({ message: 'Необходима авторизация' });
+  }
+
+  req.user = payload;
+  next();
+};
