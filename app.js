4a9c8da594294875b4c1cdb3885f7e38a7a7dd18
gafiyatovzz <54131452+gafiyatovzz@users.noreply.github.com>
2020-09-11 15:47:27 +0300

    change port

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 
-const { PORT = 3000 } = process.env;
+const { PORT = 4000 } = process.env;
 
 const app = express();
 
