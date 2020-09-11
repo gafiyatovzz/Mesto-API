c3ce860a4d15837b0d3bfee3ce654103cf64b2c5
gafiyatovzz <54131452+gafiyatovzz@users.noreply.github.com>
2020-09-11 11:59:28 +0300

    Централизованная обработка ошибок + Валидация запросов + логгирование

--- controllers/card.js
+++ controllers/card.js
@@ -1,59 +1,78 @@
 const Card = require('../models/Card');
-const errorHandler = require('../utils/errorHandler');
+const NotFoundError = require('../utils/NotFoundError');
 
-module.exports.createCard = (req, res) => {
+module.exports.createCard = (req, res, next) => {
   const { name, link } = req.body;
 
   Card.create({ name, link, owner: req.user._id })
-    .then((card) => res.send({ data: card }))
-    .catch((err) => errorHandler(res, err));
+    .then((card) => {
+      if (!card) {
+        throw new NotFoundError('Неверный параметр запроса.');
+      }
+      res.send({ data: card });
+    })
+    .catch(next);
 };
 
 
-module.exports.getCards = (req, res) => {
+module.exports.getCards = (req, res, next) => {
   Card.find({})
-    .then((card) => res.send({ data: card }))
-    .catch((err) => errorHandler(res, err));
+    .then((card) => {
+      if (!card) {
+        throw new NotFoundError('Неверный параметр запроса.');
+      }
+      res.send({ data: card });
+    })
+    .catch(next);
 };
 
 
-module.exports.deleteCard = (req, res) => {
+module.exports.deleteCard = (req, res, next) => {
   Card.findById(req.params.id)
     .then((card) => {
-      if (card === null) {
-        res.status(404).send({ message: 'Такой карточки не существует' });
-      }
-      return card;
-    })
-    .then((card) => {
-      if (card.owner == req.user._id) {
-        Card.findByIdAndRemove(card._id)
-          .then(() => res.send({ message: 'Карточка удалена!' }));
+      // eslint-disable-next-line eqeqeq
+      if (card.owner.toString() === req.user._id) {
+        Card.findByIdAndRemove(card._id, () => {
+          res.send({ message: 'Карточка удалена!' });
+        });
+      } else if (card === null) {
+        throw new NotFoundError('Такой карточки не существует');
       } else {
         res.status(401).send({ message: 'Запрещено удалять чужие карты' });
       }
     })
-    .catch((err) => errorHandler(res, err));
+    .catch(next);
 };
 
-// Promise.reject(res.status(404).json({ message: 'Карточка не найдена!' })
-module.exports.addLike = (req, res) => {
+module.exports.addLike = (req, res, next) => {
   Card.findByIdAndUpdate(
     req.params.id,
     { $addToSet: { likes: [req.user._id] } },
     { new: true },
   )
-    .then((card) => (!card ? Promise.reject(res.status(404).json({ message: 'Карточка не найдена!' })) : res.send({ data: card })))
-    .catch((err) => errorHandler(res, err));
+    .then((card) => {
+      if (!card) {
+        Promise.reject(new NotFoundError('Карточка не найдена!'));
+      }
+
+      res.send({ data: card });
+    })
+    .catch(next);
 };
 
 
-module.exports.disLike = (req, res) => {
+module.exports.disLike = (req, res, next) => {
   Card.findByIdAndUpdate(
     req.params.id,
     { $pull: { likes: req.user._id } },
     { new: true },
   )
-    .then((card) => (!card ? Promise.reject(res.status(404).json({ message: 'Карточка не найдена!' })) : res.send({ data: card })))
-    .catch((err) => errorHandler(res, err));
+    .then((card) => {
+      if (!card) {
+        throw new NotFoundError('Карточка не найдена!');
+      }
+
+      res.send({ data: card });
+    })
+    .catch(next);
 };
--- routes/user.js
+++ routes/user.js
@@ -1,14 +1,48 @@
 const express = require('express');
-const controller = require('../controllers/user');
 
 const router = express.Router();
+const controller = require('../controllers/user');
 
-router.get('/:id', controller.getById);
+const { celebrate, Joi } = require('../node_modules/celebrate');
 
-router.get('/', controller.getAll);
 
-router.patch('/me', controller.updateUser);
+router.get('/:id', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30)
+      .alphanum(),
+    about: Joi.string().required().min(2).max(30)
+      .alphanum(),
+    avatar: Joi.string().required(),
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+  }),
+}), controller.getById);
 
-router.patch('/me/avatar', controller.updateUserAvatar);
+router.get('/', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30)
+      .alphanum(),
+    about: Joi.string().required().min(2).max(30)
+      .alphanum(),
+    avatar: Joi.string().required(),
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+  }),
+}), controller.getAll);
+
+router.patch('/me', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30)
+      .alphanum(),
+    about: Joi.string().required().min(2).max(30)
+      .alphanum(),
+  }),
+}), controller.updateUser);
+
+router.patch('/me/avatar', celebrate({
+  body: Joi.object().keys({
+    avatar: Joi.string().required(),
+  }),
+}), controller.updateUserAvatar);
 
 module.exports = router;
